3ff365975502d9a09c903c7843003e91a8178b32
Felix Wong <33945353+felix0110@users.noreply.github.com>
2025-10-13 17:39:52 +0800

    Update index.tsx

--- storefront/src/modules/account/components/register/index.tsx
+++ storefront/src/modules/account/components/register/index.tsx
@@ -70,7 +70,7 @@ const Register = ({ setCurrentView }: Props) => {
         </div>
         <ErrorMessage error={message} data-testid="register-error" />
         <span className="text-center text-ui-fg-base text-small-regular mt-6">
-          By creating an account, you agree to Medusa Store&apos;s{" "}
+          By creating an account, you agree to Unity Store&apos;s{" "}
           <LocalizedClientLink
             href="/content/privacy-policy"
             className="underline"
